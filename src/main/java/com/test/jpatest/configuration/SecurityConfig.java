dea27ff3afdc5a9f8d43252f2edfb162fa007b32
aamar <aamar@gap.com>
2024-06-18 13:09:49 +0530

    KAFKA techx ending stuff and udemy changes (master must contain working code) -- make sure to comment out the spring security dependencies to run the master code (if build.gradle has those files).

--- src/main/java/com/test/jpatest/configuration/SecurityConfig.java
+++ src/main/java/com/test/jpatest/configuration/SecurityConfig.java
@@ -1,30 +1,30 @@
 package com.test.jpatest.configuration;
 
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.web.SecurityFilterChain;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.http.HttpMethod;
+//import org.springframework.security.config.Customizer;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//import static org.springframework.security.config.Customizer.withDefaults;
 
-import static org.springframework.security.config.Customizer.withDefaults;
-
-@Configuration
-@EnableWebSecurity
+//@Configuration
+//@EnableWebSecurity
 public class SecurityConfig {
 
-    @Bean
-    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
-        return http
-                .authorizeHttpRequests(authCustomizer -> authCustomizer
-                        .requestMatchers(HttpMethod.DELETE, "/deleteBooking/{bookingId}").hasRole("admin")
-                        .requestMatchers(HttpMethod.GET, "/allBookings").permitAll()
-                        .requestMatchers("/**").authenticated()).build();
-    }
+//    @Bean
+//    public SecurityFilterChain securityFilterChain (HttpSecurity http) throws Exception {
+//        return http
+//                .authorizeHttpRequests(authCustomizer -> authCustomizer
+//                        .requestMatchers(HttpMethod.DELETE, "/deleteBooking/{bookingId}").hasRole("admin")
+//                        .requestMatchers(HttpMethod.GET, "/allBookings").permitAll()
+//                        .requestMatchers("/**").authenticated()).build();
+//    }
 
 
 }
